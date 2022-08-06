ea17ba410171ce030277715a17ee55925da68c1a
Andrew-L-Campbell <83891416+Andrew-L-Campbell@users.noreply.github.com>
2022-08-05 22:16:38 -0600

    done

---
+++ js/models/mock_team_data.js
@@ -0,0 +1,44 @@
+//MOCK TEAM DATA-This should reflect your league theme.  Feel free to customize for your needs
+
+let mockTeamData=[
+    {
+      "id": 0,
+      "name":"Smokin' Hot Grillers",        //note, I use the list.nameCol in the view model to refer to this column to display team names generically.
+      "coachName": "Miranda Keyes",
+      "coachPhone": "801-444-5555",
+      "numPlayers": 4,
+      "teamPhoto": "images/all-smoked-out.jpg"      //note, I use the list.logoCol in the view model to refer to this column to display team logos generically (if used)
+    },
+    {
+      id: 1,
+      "name":"Team Traeger",
+      "coachName": "Avery Johnson",
+      "coachPhone": "801-444-6666",
+      "numPlayers": 5,
+      "teamPhoto": "images/all-smoked-out.jpg"
+    },
+    {
+      id: 2,
+      "name":"Burger Boys",
+      "coachName": "Edward Buck",
+      "coachPhone": "801-444-7777",
+      "numPlayers": 4,
+      "teamPhoto": "images/all-smoked-out.jpg"
+    },
+    {
+      id: 3,
+      "name":"Charcoal Chaps",
+      "coachName": "Jameson Locke",
+      "coachPhone": "801-444-8888",
+      "numPlayers": 6,
+      "teamPhoto": "images/all-smoked-out.jpg"
+    },
+    {
+      id: 4,
+      "name":"Green Eggs and Ham",
+      "coachName": "Catherine Halsey",
+      "coachPhone": "801-444-8889",
+      "numPlayers": 5,
+      "teamPhoto": "images/all-smoked-out.jpg"
+    }
+  ]
